09083f5bb94d9e3b11d00aae1109c75d379875d0
Everette Esguerra <emayesguer15ra@gmail.com>
2022-12-09 18:27:44 -0800

    Tally myScore from myChar in main.js day2

--- day2/main.js
+++ day2/main.js
@@ -1,9 +1,20 @@
 let round = prompt("Enter input.");
 let myArray = round.split("\n");
 
+let myScore = 0;
 let i = 0;
 while(i < myArray.length){
     let myChar = myArray[i].charAt(2);
-    console.log(myChar);
+    switch(myChar){
+        case "X":
+            myScore += 1;
+            break;
+        case "Y":
+            myScore += 2;
+            break;
+        case "Z":
+            myScore += 3;
+    }
     i++;
 }
+console.log(myScore);
